bcdb16b93288605e4a9a2f5babc2448f093b89b8
Sebastian Molina <smolinacadena@outlook.com>
2022-07-19 16:22:32 -0500

    Fixed Voxel Shapes

--- src/main/java/dev/smolinacadena/refinedcooking/block/KitchenStationBlock.java
+++ src/main/java/dev/smolinacadena/refinedcooking/block/KitchenStationBlock.java
@@ -9,6 +9,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
@@ -18,7 +19,10 @@ import javax.annotation.Nullable;
 
 public class KitchenStationBlock extends BlockKitchen {
 
-    private static final VoxelShape SHAPE = Block.box(3, -1, 6, 13, 7.5, 13);
+    private static final VoxelShape SHAPE_NORTH = Block.box(3, -1, 6, 13, 7.5, 13);
+    private static final VoxelShape SHAPE_SOUTH = Block.box(3, -1, 3, 13, 7.5, 10);
+    private static final VoxelShape SHAPE_EAST = Block.box(3, -1, 3, 10, 7.5, 13);
+    private static final VoxelShape SHAPE_WEST = Block.box(6, -1, 3, 13, 7.5, 13);
     public static final BooleanProperty CONNECTED = BooleanProperty.create("connected");
 
     public KitchenStationBlock() {
@@ -33,7 +37,19 @@ public class KitchenStationBlock extends BlockKitchen {
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
-        return SHAPE;
+        switch (state.getValue(FACING)) {
+            case UP :
+            case DOWN :
+            case SOUTH :
+            default :
+                return SHAPE_SOUTH;
+            case NORTH :
+                return SHAPE_NORTH;
+            case WEST :
+                return SHAPE_WEST;
+            case EAST :
+                return SHAPE_EAST;
+        }
     }
 
     @Nullable
